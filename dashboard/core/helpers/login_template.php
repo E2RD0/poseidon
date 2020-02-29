762a74a879b00064e9caed93ef7c5ccd711f355c
Bryan Galdámez <josuegalre@gmail.com>
2020-02-29 17:58:40 -0600

    dashboard: Agregado el progreso del dashboard.

---
+++ dashboard/core/helpers/login_template.php
@@ -0,0 +1,39 @@
+<?php
+
+class loginTemplate
+{
+    public static function loginHead($title)
+    {
+        echo(
+            '<!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="x-ua-compatible" content="ie=edge">
+                <title>'.$title.'</title>
+                <link rel="manifest" href="site.webmanifest">
+                <link rel="stylesheet" href="../resources/fonts/fonts.css">
+                <link rel="stylesheet" href="../resources/css/normalize.css">
+                <link rel="stylesheet" href="../resources/css/bootstrap.min.css">
+                <link rel="stylesheet" href="../resources/css/main.css">
+                <link rel="stylesheet" href="../resources/css/custom.css">
+                <meta name="theme-color" content="#fafafa">
+            </head>
+
+            <body>'
+        );
+    }
+
+    public static function loginEnd()
+    {
+        echo(
+            '<script src="../resources/js/vendor/jquery-3.4.1.min.js"></script>
+            <script src="../resources/js/vendor/bootstrap.bundle.min.js"></script>
+            <script src="../resources/js/plugins.js"></script>
+            <script src="../resources/js/main.js"></script>
+            </body>
+            </html>'
+        );
+    }
+}
---
+++ dashboard/views/recuperar-clave.php
@@ -0,0 +1,37 @@
+<?php
+require_once('../core/helpers/login_template.php');
+loginTemplate::loginHead("Recuperar contraseña");
+?>
+
+<main class="main__bg">
+    <div class="row justify-content-md-end m-0">
+        <div class="main__login">
+            <div class="row justify-content-center m-0">
+                <div class="col-12 recover__form text-center">
+                    <img src="../resources/img/poseidon-l.svg" alt="logo poseidon" class="img-fluid">
+                    <p class="recover__message text-break">Ingreso tu correo electrónico
+                    y te enviaremos un enlace para reestablecer tu contraseña</p>
+                </div>
+                <div class="col-8">
+                    <form action="">
+                        <div class="row main__rows">
+                            <div class="col-sm">
+                                <div class="form-field">
+                                    <div class="form-field__control">
+                                        <label for="correo" class="form-field__label">Correo electrónico</label>
+                                        <input id="correo" type="email" class="form-field__input" required/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php
+loginTemplate::loginEnd();
+?>
