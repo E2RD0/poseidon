f31cff274bec0c894a223f685490ce9fe5d2a7a4
Eduardo Estrada <eduardodavidestrada1@gmail.com>
2020-05-19 19:36:14 -0600

    reestructuración de carpetas

--- website/app/views/tienda/functions.php
+++ website/app/views/tienda/functions.php
@@ -5,14 +5,14 @@ class template {
     public static function getHeader($title, $isIndex = false) {
         $s_title = $title;
         $s_index = $isIndex;
-        require_once('templates/header.php');
+        require_once(__DIR__ . 'templates/header.php');
     }
 
     public static function getFooter() {
-        require_once('templates/footer.php');
+        require_once(__DIR__ . 'templates/footer.php');
     }
 
     public static function getSimpleFooter() {
-        require_once('templates/footer-simple.php');
+        require_once(__DIR__ . 'templates/footer-simple.php');
     }
 }
